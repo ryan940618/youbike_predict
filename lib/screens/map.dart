b10c9a4968c4f2fe967bd0932d87f0ab590ed1f6
ryan940618 <ryan940618@gmail.com>
2025-04-19 10:40:44 +0800

    Added map page

---
+++ lib/screens/map.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key, required this.title});
+  final String title;
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: const Color(0xFF292929),
+        titleTextStyle: const TextStyle(
+          color: Color(0xffa3a3a3),
+          fontSize: 18,
+        ),
+      ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: FlutterMap(
+              options: const MapOptions(
+                initialCenter: LatLng(23.761, 120.958),
+                initialZoom: 7.5,
+              ),
+              children: [
+                Container(
+                  color: const Color(0xFF292929),
+                ),
+                TileLayer(
+                  urlTemplate:
+                      'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga',
+                ),
+              ],
+            ),
+          ),
+          const Positioned(
+            left: 10,
+            bottom: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '更新時間：',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "timestamp",
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
