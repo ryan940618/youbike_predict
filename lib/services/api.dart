ff1abddb3d9adf8c609fe7483100c7a343c46c4b
ryan940618 <ryan940618@gmail.com>
2025-04-19 11:55:43 +0800

    Added api funstions for single station

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -12,3 +12,34 @@ Future<List<Station>> fetchStations() async {
   }
   return stations;
 }
+
+Future<Map<String, dynamic>> fetchStationInfo(double lat, double lon) async {
+  final url = Uri.parse("https://apis.youbike.com.tw/tw2/parkingInfo");
+  final response = await http.post(
+    url,
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode({
+      'lat': lat,
+      'lng': lon,
+      'maxDistance': 1,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('獲取站點資訊失敗');
+  }
+}
+
+Future<List<Map<String, dynamic>>> fetchStationDetail(String stationNo) async {
+  final url = Uri.parse("https://apis.youbike.com.tw/api/front/bike/lists?station_no=$stationNo");
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final jsonData = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(jsonData['retVal']);
+  } else {
+    throw Exception('獲取站點詳細資訊失敗');
+  }
+}
