07f71f65f19344d35011c7c1d6281ecb35d75f3e
ryan940618 <ryan940618@gmail.com>
2025-04-22 17:02:53 +0800

    Added histogram widget

---
+++ lib/widgets/histogram.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HourlyHistogram extends StatelessWidget {
+  final Map<int, double> data;
+  final String title;
+  final Color color;
+
+  const HourlyHistogram({
+    super.key,
+    required this.data,
+    this.title = "每小時統計",
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final hours = List.generate(24, (i) => i);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: hours.length,
+            itemBuilder: (context, index) {
+              final hour = hours[index];
+              final value = data[hour] ?? 0;
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: value,
+                      color: color,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(hour.toString()),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
