f8444d7069561b137aae4cd1713ad9ca0403eb53
ryan940618 <ryan940618@gmail.com>
2025-04-22 23:25:59 +0800

    Remove the head of each station name

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,20 +3,27 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future<List<Station>> fetchStations() async {
-  final res = await http.get(Uri.parse("https://api.kcg.gov.tw/api/service/Get/b4dd9c40-9027-4125-8666-06bef1756092"));
+  final res = await http.get(Uri.parse(
+      "https://api.kcg.gov.tw/api/service/Get/b4dd9c40-9027-4125-8666-06bef1756092"));
   final data = json.decode(res.body);
   List<Station> stations = [];
 
-  for (var item in data['data']['data']['retVal']) {
+  for (var item in data['data']['data']['retVal'].values) {
+    item['stationName'] = (item['stationName'] ?? '')
+        .toString()
+        .replaceAll('YouBike2.0_', '')
+        .trim();
+
     stations.add(Station.listJson(item));
   }
   print("初始化站點 共${stations.length}站");
   return stations;
 }
 
-Future<Map<String, dynamic>> fetchStationInfo(double lat, double lon, [int dist = 1]) async {
-  String latSafe = lat.toStringAsFixed(5); 
-  String lonSafe = lon.toStringAsFixed(5); 
+Future<Map<String, dynamic>> fetchStationInfo(double lat, double lon,
+    [int dist = 1]) async {
+  String latSafe = lat.toStringAsFixed(5);
+  String lonSafe = lon.toStringAsFixed(5);
   print("取得資料：${latSafe}, ${lonSafe}, ${dist}");
   final url = Uri.parse("https://apis.youbike.com.tw/tw2/parkingInfo");
   final response = await http.post(
@@ -37,7 +44,8 @@ Future<Map<String, dynamic>> fetchStationInfo(double lat, double lon, [int dist 
 }
 
 Future<List<Map<String, dynamic>>> fetchStationDetail(String stationNo) async {
-  final url = Uri.parse("https://apis.youbike.com.tw/api/front/bike/lists?station_no=$stationNo");
+  final url = Uri.parse(
+      "https://apis.youbike.com.tw/api/front/bike/lists?station_no=$stationNo");
   final response = await http.get(url);
 
   if (response.statusCode == 200) {
