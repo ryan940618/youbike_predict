85a68a513a4d38606f1830717e01b4461b309ca7
ryan940618 <ryan940618@gmail.com>
2025-06-18 10:00:49 +0800

    Added flask-cors to prevent CORS issue

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import json
 from datetime import datetime
 from analyzer import Analyzer
 
 app = Flask(__name__)
+CORS(app)
 
 DATASET_FOLDER = 'dataset'
 os.makedirs(DATASET_FOLDER, exist_ok=True)
